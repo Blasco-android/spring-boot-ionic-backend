7b69a12fae6760a16b13b5234bd421d4b4a0cb7b
Rodrigo Blasco <digoandroid9@gmail.com>
2020-10-14 15:38:47 -0300

    Revert "Inserting new 'Categoria' with POST"
    This reverts commit d5de91c12d1414a1cf0452ba8aa301062689c771.

--- src/main/java/com/blascoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/blascoweb/cursomc/resources/CategoriaResource.java
@@ -3,16 +3,12 @@
 package com.blascoweb.cursomc.resources;
 
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.blascoweb.cursomc.domain.Categoria;
 import com.blascoweb.cursomc.services.CategoriaService;
@@ -28,14 +24,6 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
 	}
 
 }
