a2e64d466dcdea1e0109c184f0e183791092aa1a
Rodrigo Blasco <digoandroid9@gmail.com>
2020-10-14 14:17:51 -0300

    Trocando @JsonBackReference por @JsonIgnore e excuindo @JsonManagedReference

--- src/main/java/com/blascoweb/cursomc/domain/Cliente.java
+++ src/main/java/com/blascoweb/cursomc/domain/Cliente.java
@@ -15,6 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.blascoweb.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -29,7 +31,6 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference //Cliente pode serializar Endereco(Endpoint).
 	@OneToMany(mappedBy="clientes")
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
 	
@@ -37,6 +38,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<String>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
--- src/main/java/com/blascoweb/cursomc/domain/Pagamento.java
+++ src/main/java/com/blascoweb/cursomc/domain/Pagamento.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.blascoweb.cursomc.domain.enums.StatusPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -21,7 +23,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer statusPagamento;
 	
-	
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
