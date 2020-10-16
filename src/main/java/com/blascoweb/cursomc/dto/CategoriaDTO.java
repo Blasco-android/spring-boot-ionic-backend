14bcf9de36fb92e998f1eef1138dca60008bb1fb
Rodrigo Blasco <digoandroid9@gmail.com>
2020-10-16 13:57:05 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/blascoweb/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/blascoweb/cursomc/dto/CategoriaDTO.java
@@ -4,12 +4,21 @@ package com.blascoweb.cursomc.dto;
 
 import java.io.Serializable;
 
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.blascoweb.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	//Para estas anotações foi necessario adicionar novas dependencias no pom.xml.
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="Tamanho deve ser entre 5 e 80 caracteres.")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/blascoweb/cursomc/services/CategoriaService.java
+++ src/main/java/com/blascoweb/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.blascoweb.cursomc.domain.Categoria;
+import com.blascoweb.cursomc.dto.CategoriaDTO;
 import com.blascoweb.cursomc.repositories.CategoriaRepository;
 import com.blascoweb.cursomc.services.exceptions.DataIntegrityException;
 import com.blascoweb.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(
 				page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 
