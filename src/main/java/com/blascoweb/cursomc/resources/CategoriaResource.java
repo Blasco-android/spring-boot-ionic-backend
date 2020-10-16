57e2be6ac352f5021ec36b7be201b63cec385e77
Rodrigo Blasco <digoandroid9@gmail.com>
2020-10-16 10:27:07 -0300

    Listando todas categorias com DTO

--- src/main/java/com/blascoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/blascoweb/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ package com.blascoweb.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.blascoweb.cursomc.domain.Categoria;
+import com.blascoweb.cursomc.dto.CategoriaDTO;
 import com.blascoweb.cursomc.services.CategoriaService;
 
 @RestController
@@ -53,5 +56,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	// Retorna uma lista de categoria sem os produtos
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<CategoriaDTO>> findAll() {
+			List<Categoria> list = service.findAll();
+			List<CategoriaDTO> listDto = list.stream()
+					.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+		}
 
 }
