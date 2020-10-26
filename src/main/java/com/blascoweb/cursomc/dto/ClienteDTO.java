57c3d98e908d7e602eca41b4b0d4ee51dce0a2dd
Rodrigo Blasco <digoandroid9@gmail.com>
2020-10-26 14:40:20 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/blascoweb/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/blascoweb/cursomc/dto/ClienteDTO.java
@@ -10,7 +10,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.blascoweb.cursomc.domain.Cliente;
+import com.blascoweb.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
